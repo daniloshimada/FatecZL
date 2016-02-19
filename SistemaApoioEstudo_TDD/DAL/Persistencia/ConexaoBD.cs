09d47ee06c9f96930f015f0cd4e77e98a182af88
Danilo Shimada <sdc.danilo@gmail.com>
2016-02-19 11:11:09 -0200

    Classe ConexaoBD restaurada.

---
+++ SistemaApoioEstudo_TDD/DAL/Persistencia/ConexaoBD.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace SistemaApoioEstudo.DAL.Persistencia
+{
+    public class ConexaoBD
+    {
+        private SqlParameterCollection sqlParameterCollection;
+
+        public ConexaoBD()
+        {
+            sqlParameterCollection = new SqlCommand().Parameters;
+        }
+
+        public void AdicionarParametros(string nomeParametro, object valorParametro)
+        {
+            sqlParameterCollection.AddWithValue(nomeParametro, valorParametro);
+        }
+
+        public void LimparParametros()
+        {
+            sqlParameterCollection.Clear();
+        }
+
+        public bool ExecutarManipulacao(string userStoredProcedure)
+        {
+            SqlConnection sqlConnection = new SqlConnection();
+            try
+            {
+                sqlConnection.ConnectionString = ConfigurationManager.ConnectionStrings["ConexaoSqlServer"].ConnectionString;
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandText = userStoredProcedure;
+
+                foreach (SqlParameter sqlParameter in sqlParameterCollection)
+                {
+                    sqlCommand.Parameters.AddWithValue(sqlParameter.ParameterName, sqlParameter.Value);
+                }
+                return Convert.ToBoolean(sqlCommand.ExecuteNonQuery());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                LimparParametros();
+                sqlConnection.Close();
+            }
+        }
+
+        public DataTable ExecutarConsultar(string userStoredProcedure)
+        {
+            SqlConnection sqlConnection = new SqlConnection();
+            try
+            {
+                sqlConnection.ConnectionString = ConfigurationManager.ConnectionStrings["ConexaoSqlServer"].ConnectionString;
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.CommandText = userStoredProcedure;
+
+                foreach (SqlParameter sqlParameter in sqlParameterCollection)
+                {
+                    sqlCommand.Parameters.AddWithValue(sqlParameter.ParameterName, sqlParameter.Value);
+                }
+
+                DataTable dataTable = new DataTable();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+                sqlDataAdapter.Fill(dataTable);
+                return dataTable;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                LimparParametros();
+                sqlConnection.Close();
+            }
+        }
+    }
+}
