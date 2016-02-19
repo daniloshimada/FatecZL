67dc2ecc6a6750d8d31b75fd8a5e68e79c74a33f
Danilo Shimada <sdc.danilo@gmail.com>
2016-02-19 14:09:09 -0200

    Classe NegocioUsuario restaurada.

---
+++ SistemaApoioEstudo_TDD/BLL/Negocio/NegocioUsuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaApoioEstudo.BLL.Entidades;
+
+namespace SistemaApoioEstudo.BLL.Negocio
+{
+    public class NegocioUsuario
+    {
+        public void ValidaNome(Usuario usuario)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(usuario.Nome))
+                {
+                    throw new ArgumentException("O campo nome deve ser preenchido!");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
