c22fca45474d46e75dbec628a6c5019e8a6f5f64
Danilo Shimada <sdc.danilo@gmail.com>
2016-02-24 11:41:18 -0300

    Form FormCadastrarUsuario restaurado.

---
+++ SistemaApoioEstudo_TDD/PL/FormUsuario/FormCadastrarUsuario.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SistemaApoioEstudo.BLL.Entidades;
+using SistemaApoioEstudo.BLL.Controles;
+
+namespace SistemaApoioEstudo.PL.FormUsuario
+{
+    public partial class FormCadastrarUsuario : Form
+    {
+        public FormCadastrarUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCadastrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Usuario usuario = new Usuario()
+                {
+                    Nome = textBoxNome.Text,
+                    Senha = textBoxSenha.Text
+                };
+                ControleUsuario controleUsuario = new ControleUsuario();
+                bool resultadoCadastrar = controleUsuario.Cadastrar(usuario);
+                if (resultadoCadastrar)
+                {
+                    MessageBox.Show("Usuário cadastrado com sucesso!", "SUCESSO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERRO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
