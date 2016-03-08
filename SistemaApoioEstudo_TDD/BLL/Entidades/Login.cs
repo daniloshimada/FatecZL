86082c0e866d498cc0c8af0dda738deb9a022511
Danilo Shimada <sdc.danilo@gmail.com>
2016-03-08 20:17:24 -0300

    Classe Login restaurada.

---
+++ SistemaApoioEstudo_TDD/BLL/Entidades/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaApoioEstudo.BLL.Entidades
+{
+    public static class Login
+    {
+        public static Usuario Usuario { get; set; }
+
+        public static void RegistrarUsuario(Usuario usuario)
+        {
+            Login.Usuario = usuario;
+        }
+
+        public static void RemoverUsuario()
+        {
+            Login.Usuario = null;
+        }
+    }
+}
